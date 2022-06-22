c2e09297ca1da5cf97a4364a49eb1d3213a12f0c
marinebde <boidemarine@hotmail.fr>
2022-06-22 16:11:54 +0200

    initialisation du plugin reseaux-sociaux

---
+++ wp-content/plugins/reseaux-sociaux/reseaux-sociaux.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+
+* Plugin Name: Réseaux Sociaux
+* Plugin URI: https://reseaux-sociaux.com
+* Description: Partagez vos réseaux sociaux
+* Version: 1
+* Author: Boidé Marine
+* Author URI: https://author.com
+* Text Domain: reseaux-sociaux
+
+*/
+
+// Required : Fichier fonction du plugin
+require_once plugin_dir_path(__FILE__) . 'includes/reseaux-sociaux-functions.php';
